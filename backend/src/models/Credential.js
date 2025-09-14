012101c37636399eb95386f9292fc1961c15b933
Juan Pablo Romero <romerojuanpablo.00@gmail.com>
2025-09-14 19:21:21 -0300

    CRUD

---
+++ backend/src/models/Credential.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const credentialSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  surname: { type: String, required: true },
+  document: { type: String, required: true },
+  birthdate: { type: Date, required: true },
+  memberId: { type: String, required: true, unique: true },
+  role: { 
+    type: String, 
+    enum: ['admin', 'employee', 'user'], 
+    required: true 
+  }
+}, { collection: 'digitalCredential' }); 
+
+module.exports = mongoose.model('Credential', credentialSchema);
