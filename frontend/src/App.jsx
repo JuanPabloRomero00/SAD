7c1b5dc52b9c6e160895b73d935d5e9bfbc22051
Jeanlev <perezjeanlev18@gmail.com>
2025-09-21 22:12:49 -0300

    se agrega pantalla de registro

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <main>
         <Routes>
           <Route path='/' element={<Home />}/>
-          <Route path='/singup' element={<Signup />} ></Route>
+          <Route path='/signup' element={<Signup />} ></Route>
           <Route></Route>
           <Route></Route>
         </Routes>
