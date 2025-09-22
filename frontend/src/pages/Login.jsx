e676bd891f253ec4f27e77bb2f9e3a8b1812e264
Jeanlev <perezjeanlev18@gmail.com>
2025-09-22 11:04:23 -0300

    creacion de login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import { Form, Link } from 'react-router-dom';
+
+
 
 function Login() {
   return (
-    <div>Login</div>
+    <>
+    <h2 className='formTitle'>Ingresa al Club</h2>
+    <h3 className='subTitle'>Usa tu DNI y contraseña para entrar al panel de socio.</h3>
+
+    <div className='formContainer'>
+    <div className='loginData'>
+  <div className='loginGroup'>
+    <label className='loginLabel'><strong>DNI</strong></label>
+    <input className='loginInput' label="DNI" type='number' />
+    <p className='loginText'>Ingresa solo números, sin puntos ni guiones</p>
+  </div>
+  <div className='loginGroup'>
+    <label className='loginLabel'><strong>Contraseña</strong></label>
+    <input className='loginInput' label="Altura" type='string' />
+  </div>
+   
+    <div className='loginButton'>
+        <button className='loginButton'>Ingresar</button>
+    </div>
+    </div>
+     </div>
+    <p className='formReturn'> ¿No tenés cuenta? <Link className='toLogin'> Asociate al club. </Link>para crear tu usuario</p>
+    
+
+    </>
   )
 }
 
