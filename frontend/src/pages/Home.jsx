10a3045d99c2dfe73f2819ff1871cc9b66bf8fa3
FedeO <fede.osorio@outlook.com.ar>
2025-10-21 16:01:59 -0300

    Fix estilos home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,10 +12,10 @@ function Home() {
         <div className="home-content">
           <h2>Deporte, comunidad y bienestar</h2>
           <h2 className="home-title-green">en un mismo club.</h2>
-
-          <p>Descubrí actividades para todas las edades y niveles.</p>
-          <p>Asociate online y empezá hoy a entrenar con nosotros.</p>
-
+          <div className="home-p">
+            <p>Descubrí actividades para todas las edades y niveles.</p>
+            <p>Asociate online y empezá hoy a entrenar con nosotros.</p>
+          </div>
           <div className="home-buttons">
             <Link to={"/activities"} className="btn-primary">
               Ver actividades
