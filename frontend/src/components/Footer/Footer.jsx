693ecc462d5e9f702de554f869bcd0688a01799a
FedeO <fede.osorio@outlook.com.ar>
2025-11-03 21:03:25 -0300

    Fix en estilos generales y footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -7,23 +7,16 @@ function Footer() {
       <footer className="footer">
         <div className="contenedor-footer">
           <div className="footer-col footer-brand">
-            <div className="footer-logo">
-              <div className="logoContainer">
-                <img alt="AD" className="logoImg" src="/img/ad.ico" />
-                Asociación Deportiva
-              </div>
-            </div>
             <p className="footer-text">
-              Promovemos el deporte y la vida <br /> saludable desde 1985.
-              Actividades para <br /> todas las edades.
+              Promovemos el deporte y la vida saludable desde 1985.
+              Actividades para todas las edades.
             </p>
           </div>
-          <div className="footer-col footer-nav">
-            <h3 className="footer-heading">Secciones</h3>
-            <ul className="footer-list">
-              <li>Inicio</li> <li>Actividades</li> <li>Asociate</li>
-              <li>Historia</li> <li>Contacto</li>
-            </ul>
+          <div className="footer-col">
+            <div className="footer-logo">
+              <img alt="AD" className="logoImg" src="/img/ad.ico" />
+              Asociación Deportiva
+            </div>
           </div>
           <div className="footer-col footer-contact">
             <h3 className="footer-heading">Contacto</h3>
